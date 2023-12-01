e05ad6e741f6cd90c99a2ef9d9bd5074f02f8583
rhsajibf <rakibnsajib@gmail.com>
2023-12-01 16:46:00 +0600

    Updated Site

---
+++ js/loadProjects.js
@@ -0,0 +1,65 @@
+// js/loadProjects.js
+document.addEventListener('DOMContentLoaded', function () {
+    const projectContainer = document.querySelector('.grid');
+
+    fetch('./projects/project_info.txt')
+        .then(response => response.text())
+        .then(data => {
+            const projects = data.split('\n').map((project, index) => [index + 1, ...project.split(';')]);
+
+            projects.forEach(project => {
+                // Skip processing empty lines
+                if (project.length === 1 && project[0].trim() === '') {
+                    return;
+                }
+
+                const [index, title, coverImage, ...skillsAndDescription] = project;
+                const skills = skillsAndDescription.slice(0, -2); // Exclude the links and the GitHub link
+                const githubLink = skillsAndDescription[skillsAndDescription.length - 2];
+
+                // Create project entry
+                const projectEntry = document.createElement('div');
+                projectEntry.classList.add('project-entry');
+
+                projectEntry.innerHTML = `
+                    <a href="${githubLink}" target="_blank" class="group">
+                        <img class="w-full mb-2 rounded-xl border border-gray-200 shadow-sm shadow-gray-50"
+                            src="${coverImage}" alt="Project photo">
+
+                        <p class="text-md text-gray-900 mb-1 mt-2 font-semibold group-hover:underline group-hover:underline-offset-4">
+                            ${title}
+                        </p>
+
+                        <div>
+                            ${skills.map(skill => `<button class="px-2 py-0.5 rounded-full text-xs font-sm bg-ds_blue_light text-ds_blue mr-1 mb-1">${skill}</button>`).join('')}
+                        </div>
+
+                        <p class="text-sm text-gray-600 mb-1 mt-2">${skillsAndDescription[skillsAndDescription.length - 1]}</p>
+
+                        <!-- GitHub Link -->
+                        <div class="flex items-center mt-2">
+                            <span class="mr-1 text-xs font-bold text-gray-600">View On:</span>
+                            <a href="${githubLink}" target="_blank" class="items-center">
+                                <button class="mr-2 items-center mt-0 rounded-full text-sm  border-0 text-gray-900 ml-0 py-0.5 flex px-1 hover:underline-offset-2 hover:underline hover:text-ds_blue">
+                                    <svg width="1024" height="1024" viewBox="0 0 1024 1024" class="h-4 w-4 mr-1"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" clip-rule="evenodd"
+                                            d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z"
+                                            transform="scale(64)" fill="currentColor"></path>
+                                    </svg>
+                                    GitHub
+                                </button>
+                            </a>
+                        </div>
+                    </a>
+                `;
+
+                // Append project entry to grid container
+                projectContainer.appendChild(projectEntry);
+            });
+
+            // Add event listener for the "Read More" buttons
+            // ... (unchanged) ...
+        })
+        .catch(error => console.error('Error fetching project information:', error));
+});
